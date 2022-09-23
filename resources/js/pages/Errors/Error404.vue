40f00573ca7560466f88ea962af2683fbf6e1a6e
Lê Hồng Minh <637942@sv.vnua.edu.vn>
2022-09-23 11:20:42 +0700

    add layout admin

--- resources/js/pages/Errors/Error404.vue
+++ resources/js/pages/Errors/Error404.vue
@@ -1,13 +1,44 @@
 <template>
-  <div>404</div>
+  <div class="errorImage">
+    <img src="/images/error404.png" alt="">
+
+  </div>
+  <div class="btnHomePage">
+    <q-btn unelevated rounded color="primary" label="Trở lại trang chủ" @click="redirectHomePage"/>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "Error404"
-}
+<script lang="ts">
+import {defineComponent} from 'vue'
+import {useRouter} from "vue-router/dist/vue-router";
+
+export default defineComponent({
+  name: "Error404",
+  setup() {
+    const router = useRouter()
+
+    const redirectHomePage = () => {
+      router.push({name: 'Home'})
+    }
+
+    return {
+      redirectHomePage
+    }
+  }
+})
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.errorImage {
+  width: 40vw;
+  margin: 0 auto;
 
+  img {
+    width: 100%;
+  }
+}
+
+.btnHomePage {
+  text-align: center;
+}
 </style>
