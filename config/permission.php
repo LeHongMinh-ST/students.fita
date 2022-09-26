05ac15d68f139df421e982564746f8a9ff657644
Lê Hồng Minh <637942@sv.vnua.edu.vn>
2022-09-26 11:50:35 +0700

    add migration user

---
+++ config/permission.php
@@ -0,0 +1,106 @@
+<?php
+return [
+    [
+        'code' => 'dashboard-index',
+        'name' => 'Xem Trang bảng điều khiển',
+        'group_code' => 'dashboard',
+        'description' => 'Có quyền xem Trang bảng điều khiển'
+    ],
+    [
+        'code' => 'role-index',
+        'name' => 'Xem danh sách vai trò',
+        'group_code' => 'role',
+        'description' => 'Có quyền xem danh sách vai trò'
+    ],
+    [
+        'code' => 'role-create',
+        'name' => 'Tạo mới vai trò',
+        'group_code' => 'role',
+        'description' => 'Có quyền tạo mới vai trò'
+    ],
+    [
+        'code' => 'role-read',
+        'name' => 'Xem chi tiết vai trò',
+        'group_code' => 'role',
+        'description' => 'Có quyền xem chi tiết vai trò'
+    ],
+    [
+        'code' => 'role-update',
+        'name' => 'Cập nhật vai trò',
+        'group_code' => 'role',
+        'description' => 'Có quyền cập nhật vai trò'
+    ],
+    [
+        'code' => 'role-delete',
+        'name' => 'Xoá vai trò',
+        'group_code' => 'role',
+        'description' => 'Có quyền xoá vai trò'
+    ],
+    [
+        'code' => 'user-index',
+        'name' => 'Xem danh sách người dùng',
+        'group_code' => 'user',
+        'description' => 'Có quyền xem danh sách người dùng'
+    ],
+    [
+        'code' => 'user-create',
+        'name' => 'Tạo mới người dùng',
+        'group_code' => 'user',
+        'description' => 'Có quyền tạo mới người dùng'
+    ],
+    [
+        'code' => 'user-read',
+        'name' => 'Xem chi tiết người dùng',
+        'group_code' => 'user',
+        'description' => 'Có quyền xem chi tiết người dùng'
+    ],
+    [
+        'code' => 'user-update',
+        'name' => 'Cập nhật người dùng',
+        'group_code' => 'user',
+        'description' => 'Có quyền cập nhật người dùng'
+    ],
+    [
+        'code' => 'user-delete',
+        'name' => 'Xoá người dùng',
+        'group_code' => 'user',
+        'description' => 'Có quyền xoá người dùng'
+    ],
+    [
+        'code' => 'user-reset-password',
+        'name' => 'Đổi mật khẩu người dùng',
+        'group_code' => 'user',
+        'description' => 'Có quyền cập nhật lại mật khẩu'
+    ],
+
+    [
+        'code' => 'department-index',
+        'name' => 'Xem danh sách bộ môn',
+        'group_code' => 'department',
+        'description' => 'Có quyền xem danh sách bộ môn'
+    ],
+    [
+        'code' => 'department-create',
+        'name' => 'Tạo mới bộ môn',
+        'group_code' => 'department',
+        'description' => 'Có quyền tạo mới bộ môn'
+    ],
+    [
+        'code' => 'department-read',
+        'name' => 'Xem chi tiết bộ môn',
+        'group_code' => 'department',
+        'description' => 'Có quyền xem chi tiết bộ môn'
+    ],
+    [
+        'code' => 'department-update',
+        'name' => 'Cập nhật bộ môn',
+        'group_code' => 'department',
+        'description' => 'Có quyền cập nhật bộ môn'
+    ],
+    [
+        'code' => 'department-delete',
+        'name' => 'Xoá bộ môn',
+        'group_code' => 'department',
+        'description' => 'Có quyền xóa bộ môn'
+    ],
+];
