ec82bcb45b41711c95b230e6f8a382cc25244d0d
Lê Hồng Minh <637942@sv.vnua.edu.vn>
2022-10-24 21:15:06 +0700

    add base request

--- app/Models/GeneralClass.php
+++ app/Models/GeneralClass.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
@@ -25,8 +26,8 @@ class GeneralClass extends Model
         return $this->hasMany(Student::class);
     }
 
-    public function department(): BelongsToMany
+    public function department(): BelongsTo
     {
-        return $this->belongsToMany(Department::class);
+        return $this->belongsTo(Department::class);
     }
 }
