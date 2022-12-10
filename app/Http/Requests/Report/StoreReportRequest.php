0f8d8dea01149417636f698dc6ef3f276fed8720
Lê Hồng Minh <637942@sv.vnua.edu.vn>
2022-12-11 00:10:18 +0700

    up

--- app/Http/Requests/Report/StoreReportRequest.php
+++ app/Http/Requests/Report/StoreReportRequest.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Requests\Report;
 
-use Illuminate\Foundation\Http\FormRequest;
+use App\Http\Requests\BaseRequest;
 
-class StoreReportRequest extends FormRequest
+class StoreReportRequest extends BaseRequest
 {
     /**
      * Determine if the user is authorized to make this request.
